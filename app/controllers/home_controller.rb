23d7a7b04a2ee2e9a2dffffc8f05e9b8a8b3e47f
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2019-03-06 23:35:24 +0000

    Adds geocode method to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def geocode
+    results = Geocoder.search(params[:address])
+    coordinates = results.first.coordinates
+    
+    @coordinates = { latitude: coordinates[0], longitude: coordinates[1] }
+  end
 end
